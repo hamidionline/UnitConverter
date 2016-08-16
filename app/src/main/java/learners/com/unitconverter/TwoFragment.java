7585ee0c7b1e143401eecb67288a1bef2cc7ef4b
JPatel <pateljay0323@gmail.com>
2016-08-16 19:11:22 +0530

    Learners

--- app/src/main/java/learners/com/unitconverter/TwoFragment.java
+++ app/src/main/java/learners/com/unitconverter/TwoFragment.java
@@ -2,6 +2,8 @@ package learners.com.unitconverter;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,21 +27,76 @@ public class TwoFragment extends Fragment{
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_two, container, false);
 
-        EditText bitpersecond = (EditText) view.findViewById(R.id.input_bit_per_second);
-        EditText kilobitpersecond = (EditText) view.findViewById(R.id.input_kilobit_per_second);
-        EditText kilobytepersecond = (EditText) view.findViewById(R.id.input_kilobyte_per_second);
-        EditText kibibitpersecond = (EditText) view.findViewById(R.id.input_kibibit_per_second);
-        EditText megabitpersecond = (EditText) view.findViewById(R.id.input_megabit_per_second);
-        EditText megabytepersecond = (EditText) view.findViewById(R.id.input_megabyte_per_second);
-        EditText mebibitpersecond = (EditText) view.findViewById(R.id.input_mebibit_per_second);
-        EditText gigabitpersecond = (EditText) view.findViewById(R.id.input_gigabit_per_second);
-        EditText gigabytepersecond = (EditText) view.findViewById(R.id.input_gigabyte_per_second);
-        EditText gibibitpersecond = (EditText) view.findViewById(R.id.input_gibibit_per_second);
-        EditText terabitpersecond = (EditText) view.findViewById(R.id.input_terabit_per_second);
-        EditText terabytepersecond = (EditText) view.findViewById(R.id.input_terabyte_per_second);
-        EditText tebibitpersecond = (EditText) view.findViewById(R.id.input_tebibit_per_second);
+        final EditText bitpersecond = (EditText) view.findViewById(R.id.input_bit_per_second);
+        final EditText kilobitpersecond = (EditText) view.findViewById(R.id.input_kilobit_per_second);
+        final EditText kilobytepersecond = (EditText) view.findViewById(R.id.input_kilobyte_per_second);
+        final EditText kibibitpersecond = (EditText) view.findViewById(R.id.input_kibibit_per_second);
+        final EditText megabitpersecond = (EditText) view.findViewById(R.id.input_megabit_per_second);
+        final EditText megabytepersecond = (EditText) view.findViewById(R.id.input_megabyte_per_second);
+        final EditText mebibitpersecond = (EditText) view.findViewById(R.id.input_mebibit_per_second);
+        final EditText gigabitpersecond = (EditText) view.findViewById(R.id.input_gigabit_per_second);
+        final EditText gigabytepersecond = (EditText) view.findViewById(R.id.input_gigabyte_per_second);
+        final EditText gibibitpersecond = (EditText) view.findViewById(R.id.input_gibibit_per_second);
+        final EditText terabitpersecond = (EditText) view.findViewById(R.id.input_terabit_per_second);
+        final EditText terabytepersecond = (EditText) view.findViewById(R.id.input_terabyte_per_second);
+        final EditText tebibitpersecond = (EditText) view.findViewById(R.id.input_tebibit_per_second);
+
+        bitpersecond.addTextChangedListener(new TextWatcher() {
+            int charCount = 0;
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                charCount = count;
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (bitpersecond.isFocused()) {
+                    if (s.toString().trim() != null && !s.toString().trim().isEmpty() && charCount > -1 && s.toString().trim().charAt(s.length() - 1) != '.' && tryParseDouble(s.toString().trim())) {
+                        kilobitpersecond.setText(String.valueOf((Double.parseDouble(s.toString()) / 1000)));
+                        kilobytepersecond.setText(String.valueOf(Double.parseDouble(s.toString()) / 8000));
+                        //kibibitpersecond.setText(String.valueOf(Double.parseDouble(s.toString()) / 258.998811));
+                        megabitpersecond.setText(String.valueOf(Double.parseDouble(s.toString()) / 1000000));
+                        megabytepersecond.setText(String.valueOf(Double.parseDouble(s.toString()) / 8000000));
+                        //mebibitpersecond.setText(String.valueOf(Double.parseDouble(s.toString()) / 0.000000064516));
+                        gigabitpersecond.setText(String.valueOf(Double.parseDouble(s.toString()) / 1000000000));
+                        gigabytepersecond.setText(String.valueOf(Double.parseDouble(s.toString()) / 8000000000L));
+                        //gibibitpersecond.setText(String.valueOf(Double.parseDouble(s.toString()) / 0.404685642));
+                        terabitpersecond.setText(String.valueOf(Double.parseDouble(s.toString()) / 1000000000000L));
+                        terabytepersecond.setText(String.valueOf(Double.parseDouble(s.toString()) / 8000000000000L));
+                        //tebibitpersecond.setText(String.valueOf(Double.parseDouble(s.toString()) / 0.404685642));
+                    } else if (s.toString().trim() != null && !s.toString().trim().isEmpty() && s.toString().trim().charAt(s.length() - 1) == '.') {
+                    } else {
+                        kilobitpersecond.getText().clear();
+                        kilobytepersecond.getText().clear();
+                        kibibitpersecond.getText().clear();
+                        megabitpersecond.getText().clear();
+                        megabytepersecond.getText().clear();
+                        mebibitpersecond.getText().clear();
+                        gigabitpersecond.getText().clear();
+                        gigabytepersecond.getText().clear();
+                        gibibitpersecond.getText().clear();
+                        terabitpersecond.getText().clear();
+                        terabytepersecond.getText().clear();
+                        tebibitpersecond.getText().clear();
+                    }
+                }
+            }
+        });
 
         return view;
     }
 
+    private boolean tryParseDouble(String value) {
+        try {
+            Double.parseDouble(value);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
 }
